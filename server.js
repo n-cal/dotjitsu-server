01e152cc763d3a4220e1151b83654b1c972d6b74
n-cal <nic.calv@outlook.it>
2021-03-11 16:29:28 +0100

    change cors origin from whitelist to *

--- server.js
+++ server.js
@@ -6,18 +6,10 @@ const app = require('./src/app');
 
 const port = process.env.PORT || 3000;
 
-const whitelist = ['http://192.168.0.103:8080', 'http://192.168.0.109:4200'];
 
 const io = require('socket.io')(httpServer, {
     cors: {
-        //origin: 'http://192.168.0.103:8080'
-        origin: (origin, callback) => {
-            if(whitelist.indexOf(origin) !== -1) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS!'));
-            }
-        }
+        origin: "*"
     }
 });
 
